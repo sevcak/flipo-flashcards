3c2c4a0f65369e8e2c11567449497bea7dc71044
Lukáš Ševc <lukassevc000@gmail.com>
2022-10-22 18:31:34 +0200

    set up dependencies for Nativewind/Tailwind CSS and React Navigation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx}",
+    "./screens/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
