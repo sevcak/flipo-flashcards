35cec84bd62e502f480eafd51873b478b6049531
sevcak <sevclukas0@gmail.com>
2023-02-14 20:42:20 +0100

    exampleDecks file: fixed 4 spaced indentd to 2

--- default_data/example-decks.js
+++ default_data/example-decks.js
@@ -1,88 +1,88 @@
 export default [
-    {
-        'id': 0,
-        'custom': false,
-        'title': 'Capitals of countries',
-        'coverUrl': require('../assets/example-decks/capitals-of-countries/cover.jpg'),
-        'cards': [
-            {
-                'id': 0,
-                'ars': 1,
-                'front': {
-                    'title': 'Germany',
-                    'content': 'What is the capital of Germany?',
-                    'image': undefined,
-                }, 
-                'back': {
-                    'title': 'Berlin',
-                    'content': undefined,
-                    'image': undefined,
-                }
-            },
-            {
-                'id': 1,
-                'ars': 1,
-                'front': {
-                    'title': 'Czech Republic',
-                    'content': 'What is the capital of Czech Republic?',
-                    'image': undefined,
-                }, 
-                'back': {
-                    'title': 'Prague',
-                    'content': undefined,
-                    'image': undefined,
-                }
-            },
-            {
-                'id': 2,
-                'ars': 1,
-                'front': {
-                    'title': 'Austria',
-                    'content': 'What is the capital of Austria?',
-                    'image': undefined,
-                }, 
-                'back': {
-                    'title': 'Vienna',
-                    'content': undefined,
-                    'image': require('../assets/example-decks/capitals-of-countries/cards/vienna.jpg'),
-                }
-            },
-        ]
-    },
-    {
-        'id': 1,
-        'custom': false,
-        'title': 'Continents of countries',
-        'coverUrl': require('../assets/example-decks/continents-of-countries/cover.jpg'),
-        'cards': [
-            {
-                'id': 0,
-                'ars': 1,
-                'front': {
-                    'title': 'Germany',
-                    'content': 'On which continent is Germany located?',
-                    'image': undefined,
-                }, 
-                'back': {
-                    'title': 'Europe',
-                    'content': undefined,
-                    'image': undefined,
-                }
-            },
-            {
-                'id': 1,
-                'ars': 1,
-                'front': {
-                    'title': 'Czech Republic',
-                    'content': 'On which continent is Czech Republic located?',
-                    'image': undefined,
-                }, 
-                'back': {
-                    'title': 'Europe',
-                    'content': undefined,
-                    'image': undefined,
-                }
-            },
-        ]
-    },
-]
+  {
+    id: 0,
+    custom: false,
+    title: "Capitals of countries",
+    coverUrl: require("../assets/example-decks/capitals-of-countries/cover.jpg"),
+    cards: [
+      {
+        id: 0,
+        ars: 1,
+        front: {
+          title: "Germany",
+          content: "What is the capital of Germany?",
+          image: undefined,
+        },
+        back: {
+          title: "Berlin",
+          content: undefined,
+          image: undefined,
+        },
+      },
+      {
+        id: 1,
+        ars: 1,
+        front: {
+          title: "Czech Republic",
+          content: "What is the capital of Czech Republic?",
+          image: undefined,
+        },
+        back: {
+          title: "Prague",
+          content: undefined,
+          image: undefined,
+        },
+      },
+      {
+        id: 2,
+        ars: 1,
+        front: {
+          title: "Austria",
+          content: "What is the capital of Austria?",
+          image: undefined,
+        },
+        back: {
+          title: "Vienna",
+          content: undefined,
+          image: require("../assets/example-decks/capitals-of-countries/cards/vienna.jpg"),
+        },
+      },
+    ],
+  },
+  {
+    id: 1,
+    custom: false,
+    title: "Continents of countries",
+    coverUrl: require("../assets/example-decks/continents-of-countries/cover.jpg"),
+    cards: [
+      {
+        id: 0,
+        ars: 1,
+        front: {
+          title: "Germany",
+          content: "On which continent is Germany located?",
+          image: undefined,
+        },
+        back: {
+          title: "Europe",
+          content: undefined,
+          image: undefined,
+        },
+      },
+      {
+        id: 1,
+        ars: 1,
+        front: {
+          title: "Czech Republic",
+          content: "On which continent is Czech Republic located?",
+          image: undefined,
+        },
+        back: {
+          title: "Europe",
+          content: undefined,
+          image: undefined,
+        },
+      },
+    ],
+  },
+];
