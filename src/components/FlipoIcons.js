63a62d34a99f6dc1e510fd96afc37ad5ab0875fe
sevcak <sevclukas0@gmail.com>
2023-04-08 16:13:18 +0200

    feat: add homepage

---
+++ src/components/FlipoIcons.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FlipoIcons = () => {
+  return (
+    <div>
+        <img alt='flipo' src='./../assets/logo/flipo-icon-light.svg'></img>
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav>
+      Flipo flashcards
+    </nav>
+  );
+}
+
+export default Header;
