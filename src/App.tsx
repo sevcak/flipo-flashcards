2a280bae118d5ffd4395d3ba3f1fc8021137bd68
sevcak <sevclukas0@gmail.com>
2023-04-11 11:40:08 +0200

    feat: make header fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import ContributePage from './pages/contribute/ContributePage';
+import DownloadPage from './pages/download/DownloadPage';
+import PrivacyPolicyPage from './pages/privacy-policy/PrivacyPolicyPage';
 
 function App(): JSX.Element {
   return (
-    <div>
+    <div className='text-secondary'>
       <Header />
+      <div className='pt-10'>
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+            <Route path="/contribute" element={<ContributePage />} />
+            <Route path="/download" element={<DownloadPage />}/>
+          </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,32 @@
 import React from 'react';
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
 import Icon from './Icon';
-import PrivacyPolicyPage from '../pages/privacy-policy/PrivacyPolicyPage';
 import { NavLink } from 'react-router-dom';
-import HomePage from '../pages/home/HomePage';
-import DownloadPage from '../pages/download/DownloadPage';
-import ContributePage from '../pages/contribute/ContributePage';
 
 const Header = () => {
   return (
-    <div>
-      <nav
-        className='
-          flex flex-row justify-between items-center
-          py-4 px-6
-          border-b-ui border-b-2
-          text-ui
-        '
-      >
-        <NavLink to="/" className='flex flex-row space-x-2 items-center'>
-          <Icon name='flipo' color='light' size={30}/>
-          <div className='text-secondary font-semibold'>
-            flipo flashcards
+      <div className='backdrop-blur-lg fixed w-full'>
+        <nav
+          className='
+            flex flex-row justify-between items-center
+            py-4 px-6
+            border-b-ui border-b-2
+            text-ui
+            bg-primary/70 backdrop-opacity-30
+          '
+        >
+          <NavLink to="/" className='flex flex-row space-x-2 items-center'>
+            <Icon name='flipo' color='light' size={30}/>
+            <div className='text-secondary font-semibold'>
+              flipo flashcards
+            </div>
+          </NavLink>
+          <div className='space-x-8 font-semibold'>
+            <NavLink to="/download">Download</NavLink>
+            <NavLink to="/contribute">Contribute</NavLink>
+            <NavLink to='/privacy-policy'>Privacy policy</NavLink>
           </div>
-        </NavLink>
-        <div className='space-x-8 font-semibold'>
-          <NavLink to="/download">Download</NavLink>
-          <NavLink to="/contribute">Contribute</NavLink>
-          <NavLink to='/privacy-policy'>Privacy policy</NavLink>
-        </div>
-      </nav>
-      <div>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route path="/contribute" element={<ContributePage />} />
-            <Route path="/download" element={<DownloadPage />}/>
-          </Routes>
+        </nav>
       </div>
-    </div>
   );
 }
 
