95df92576bc0cedf8124eb2220a65a5f69224612
sevcak <sevclukas0@gmail.com>
2023-04-11 07:53:50 +0200

    fix: fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App(): JSX.Element {
   return (
     <div>
       <Header />
-      <HomePage />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Icon from './Icon';
 import PrivacyPolicyPage from '../pages/privacy-policy/PrivacyPolicyPage';
+import { NavLink } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
 
 const Header = () => {
   return (
@@ -15,27 +17,31 @@ const Header = () => {
           text-ui
         '
       >
-        <a href="./flipo-flashcards" className='flex flex-row space-x-2 items-center'>
+        <a href="/" className='flex flex-row space-x-2 items-center'>
           <Icon name='flipo' color='light' size={30}/>
           <div className='text-secondary font-semibold'>
             flipo flashcards
           </div>
         </a>
+        {/* <NavLink to='/'>
+          <Icon name='flipo' color='light' size={30}/>
+          <div className='text-secondary font-semibold'>
+            flipo flashcards
+          </div>
+        </NavLink> */}
         <div className='space-x-8 font-semibold'>
-          <a href="./flipo-flashcards/download">Download</a>
-          <a href="./flipo-flashcards/contribute">Contribute</a>
-          <a href="./flipo-flashcards/privacy-policy">Privacy policy</a>
+          <NavLink to="/download">Download</NavLink>
+          <NavLink to="/contribute">Contribute</NavLink>
+          <NavLink to='/privacy-policy'>Privacy policy</NavLink>
         </div>
       </nav>
       <div>
-        <BrowserRouter>
           <Routes>
-            <Route path="/" />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />}/>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
             <Route path="/contribute" />
             <Route path="/download" />
           </Routes>
-        </BrowserRouter>
       </div>
     </div>
   );
