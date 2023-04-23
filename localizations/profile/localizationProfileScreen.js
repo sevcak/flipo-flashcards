4a87f9f9b5f6363274a7ac62f048ad861db81d72
sevcak <sevclukas0@gmail.com>
2023-04-23 20:31:34 +0200

    feat: add sk and de translation to the profile screen

---
+++ localizations/profile/localizationProfileScreen.js
@@ -0,0 +1,73 @@
+const en = {
+    name: "Name",
+    guest: "Guest",
+    googleLogin: "Sign in with Google",
+    warning: "Warning",
+    privacyPolicyWarning01: "By signing in using Google you agree to our ",
+    privacyPolicy: "Privacy Policy",
+    privacyPolicyWarning02: "",
+    cancel: "Cancel",
+    googleAccount: "Google Account",
+    exportGDrive: "Export decks to Drive",
+    exportGDriveWarning01: "Exporting your local decks to your Google Drive will overwrite the decks currently stored there.",
+    wantToProceed: "Do you want to proceed?",
+    proceed: "Proceed",
+    pleaseWait: "Please Wait",
+    importGDriveWarning01: "Importing decks from your Google Drive will overwrite your local decks.",
+    exportingGDrive: "Exporting data to your Google Drive...",
+    importGDrive: "Import decks from Drive",
+    importingGDrive: "Importing data from your Google Drive...",
+    cantImport: "Can't import decks",
+    noDecksGDrive: "No custom decks were found on your Google Drive.",
+    googleSignOut: "Sign out of Google"
+};
+
+const sk = {
+    name: "Meno",
+    guest: "Hosť",
+    googleLogin: "Prihlásiť sa pomocou Google",
+    warning: "Varovanie",
+    privacyPolicyWarning01: "Prihlásením sa pomocou Google súhlasíte s našimi ",
+    privacyPolicy: "Zásadami ochrany osobných údajov",
+    privacyPolicyWarning02: "",
+    cancel: "Zrušiť",
+    googleAccount: "Účet Google",
+    exportGDrive: "Exportovať balíčky na Disk",
+    exportGDriveWarning01: "Exportovanie lokálnych balíčkov na váš Disk Google prepíše balíčky, ktoré sú tam aktuálne uložené.",
+    wantToProceed: "Chcete pokračovať?",
+    proceed: "Pokračovať",
+    pleaseWait: "Prosím počkajte",
+    importGDriveWarning01: "Importovanie balíčkov z vášho Disku Google prepíše vaše lokálne balíčky.",
+    exportGDrive: "Exportujeme údaje na váš Disk Google...",
+    importGDrive: "Importovať balíčky z Disku",
+    importingGDrive: "Importujeme údaje z vášho Disku Google...",
+    cantImport: "Nemožno importovať balíčky",
+    noDecksGDrive: "Na vašom disku Google neboli nájdené žiadne balíčky.",
+    googleSignOut: "Odhlásiť sa zo služby Google"
+};
+
+const de = {
+    name: "Name",
+    guest: "Gast",
+    googleLogin: "Mit Google anmelden",
+    warning: "Warnung",
+    privacyPolicyWarning01: "Wenn Sie sich mit Google anmelden, stimmen Sie unseren ",
+    privacyPolicy: "Datenschutzbestimmungen",
+    privacyPolicyWarning02: " zu.",
+    cancel: "Abbrechen",
+    googleAccount: "Google-Konto",
+    exportGDrive: "Decks in Drive exportieren",
+    exportGDriveWarning01: "Wenn Sie Ihre lokalen Decks in Ihr Google Drive exportieren, werden die derzeit dort gespeicherten Decks überschrieben.",
+    wantToProceed: "Möchten Sie fortfahren?",
+    proceed: "Fortfahren",
+    pleaseWait: "Bitte warten Sie",
+    importGDriveWarning01: "Der Import von Decks aus Ihrem Google Drive überschreibt Ihre lokalen Decks.",
+    exportingGDrive: "Exportieren von Daten in Ihr Google Drive...",
+    importGDrive: "Decks aus Drive importieren",
+    importingGDrive: "Importieren von Daten aus Ihrem Google Drive...",
+    cantImport: "Decks können nicht importiert werden",
+    noDecksGDrive: "Es wurden keine benutzerdefinierten Decks in Ihrem Google Drive gefunden.",
+    googleSignOut: "Von Google abmelden"
+};
+
+export { en, sk, de };
