4dbd9b61b1306471faff461ec75d6c6d876e10e9
sevcak <sevclukas0@gmail.com>
2023-04-23 21:39:34 +0200

    feat: add sk and de translation for DeckEditScreen screen

---
+++ localizations/decks/localizationDeckEditScreen.js
@@ -0,0 +1,67 @@
+const en = {
+    screenTitleNew: "New Deck",
+    screenTitleEdit: "Edit Deck",
+    deckName: "Deck Name:",
+    create: "Create",
+    addCards: "Add cards",
+    newCard: "New Card",
+    card: "Card",
+    front: "Front",
+    frontTitle: "Front title",
+    frontContent: "Front content",
+    flip: "flip",
+    back: "back",
+    backTitle: "Back title",
+    backContent: "Back content",
+    confirm: "Confirm",
+    cantCreate: "Can't create deck",
+    toCreate: "To create a deck it has to have at least two cards.",
+    deleteDeck: "Delete deck",
+    done: "Done"
+};
+
+const sk = {
+    screenTitleNew: "Nový balíček",
+    screenTitleEdit: "Úprava balíčka",
+    deckName: "Názov balíčka:",
+    create: "Vytvoriť",
+    addCards: "Pridať karty",
+    newCard: "Nová karta",
+    card: "Karta",
+    front: "Predná strana",
+    frontTitle: "Predný názov",
+    frontContent: "Predný obsah",
+    flip: "Otočiť",
+    back: "zadná strana",
+    backTitle: "Zadný názov",
+    backContent: "Zadný obsah",
+    potvrdiť: "Potvrdiť",
+    cantCreate: "Nemožno vytvoriť balíček",
+    toCreate: "Ak chcete vytvoriť balíček, musí mať aspoň dve karty.",
+    deleteDeck: "Vymazať balíček",
+    done: "Hotovo"
+};
+
+const de = {
+    screenTitleNew: "Neues Deck",
+    screenTitleEdit: "Deck bearbeiten",
+    deckName: "Deckname:",
+    create: "Erstellen",
+    addCards: "Karten hinzufügen",
+    newCard: "Neue Karte",
+    card: "Karte",
+    front: "Vorderseite",
+    frontTitle: "Vorderseite Titel",
+    frontInhalt: "Vorderer Inhalt",
+    flip: "Spiegeln",
+    back: "Rückseite",
+    backTitle: "Hinterer Titel",
+    backContent: "Inhalt hinten",
+    confirm: "Bestätigen",
+    cantCreate: "Deck kann nicht erstellt werden",
+    toCreate: "Um ein Deck zu erstellen, muss es mindestens zwei Karten haben.",
+    deleteDeck: "Deck löschen",
+    done: "Fertig"
+};
+
+export { en, sk, de };
